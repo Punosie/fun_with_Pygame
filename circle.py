a4f8d2063570d287e7a031e4a51ea30bc11902e3
Shubhabha <shubhankar.kaushik2003@gmail.com>
2024-04-10 23:57:08 +0530

    Created Moving circle

---
+++ circle.py
@@ -0,0 +1,38 @@
+import pygame
+
+#Setup
+pygame.init()
+screen_width = 1280
+screen_height = 720
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill('Black')
+
+    pygame.draw.circle(screen, 'pink', player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
+    pygame.display.flip()
+
+    dt = clock.tick(60)/1000
+
+
+pygame.quit()
